907350f60eae0b62df0a5648528c8e69759393ce
thgzr <thgzrr@gmail.com>
2025-07-29 06:26:56 +0000

    removed HTML representation, trimmed unused functions for CRUD

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import meals, ingredients, meal_ingredients, ingredients_html
+from app.routers import meal_ingredient, meals, ingredients, ingredients_html
 from fastapi.staticfiles import StaticFiles
 
 from fastapi.templating import Jinja2Templates
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.include_router(meals.router)
 app.include_router(ingredients.router)
-app.include_router(meal_ingredients.router)
+app.include_router(meal_ingredient.router)
 app.include_router(ingredients_html.router)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
